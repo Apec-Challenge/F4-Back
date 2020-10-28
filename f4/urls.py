6d4cedc75c79ce630def2c629e26834d6a8f606d
LAPTOP-JHR4BH9T\My Notebook <jinsol0330@sch.ac.kr>
2020-10-28 18:53:10 +0900

    기능::장소 기반 리뷰 조회 기능 추가

--- f4/urls.py
+++ f4/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 from rest_framework import routers
 from accounts import urls as acc_urls
 from funding.views import FundingUpdateAPIView, FundingDeleteAPIView, FundingCreateViewSet, FundingViewSet
-from review.viewsets import ReviewListViewSet, ReviewCreateViewSet, ReviewUpdateViewSet, ReviewDeleteViewSet
+from review.viewsets import ReviewListViewSet, ReviewUpdateViewSet, ReviewDeleteViewSet
 from accounts import views as acc_views
 from allauth.account.views import confirm_email
 from django.conf.urls.static import static
@@ -30,25 +30,17 @@ from place.viewsets import PlaceViewSet,PlaceDeleteAPIView, PlaceCreateAPIView, 
 router = routers.DefaultRouter()
 router.register('funding', FundingViewSet, basename='funding')
 router.register('place',PlaceViewSet,basename='place')
-
-
-router = routers.DefaultRouter()
 router.register('review', ReviewListViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(acc_urls)),
     path('api/', include(router.urls)),
-    # url('api/review/create/$', ReviewCreateViewSet.as_view(), name='review_create'),
     url('api/review/update/(?P<id>[\w-]+)/$', ReviewUpdateViewSet.as_view(), name='review_update'),
     url('api/review/delete/(?P<id>[\w-]+)/$', ReviewDeleteViewSet.as_view(), name='review_delete'),
     url('api/funding/(?P<id>[\w-]+)/edit/$', FundingUpdateAPIView.as_view(), name='funding_update'),
     url('api/funding/(?P<id>[\w-]+)/delete/$', FundingDeleteAPIView.as_view(), name='funding_delete'),
     # url('api/funding/create/$', FundingCreateViewSet.as_view(), name='funding_create'),
-    # url('api/review/$', ReviewListViewSet.as_view(), name='review'),
-    url('api/review/(?P<id>[\w-]+)/create/$', ReviewCreateViewSet.as_view(), name='review_create'),
-    url('api/review/(?P<id>[\w-]+)/update/$', ReviewUpdateViewSet.as_view(), name='review_update'),
-    url('api/review/(?P<id>[\w-]+)/delete/$', ReviewDeleteViewSet.as_view(), name='review_delete'),
     path('accounts/', include('allauth.urls')),
     url(r'^rest-auth/', include('rest_auth.urls')),
     url(r'^rest-auth/registration/', include('rest_auth.registration.urls')),
