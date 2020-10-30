ceaa298c290125533e31511a3b772dc97ffe430d
LAPTOP-JHR4BH9T\My Notebook <jinsol0330@sch.ac.kr>
2020-10-30 16:25:10 +0900

    기능::Review API PATCH 기능 구현중, Model 일부 수정(진솔)

--- f4/settings.py
+++ f4/settings.py
@@ -133,20 +133,13 @@ WSGI_APPLICATION = 'f4.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'f4',
         'USER': 'root',
-        'PASSWORD': '080799',
+        'PASSWORD': '111111',
         'HOST': 'localhost',
         'PORT': '3306',
     }
--- review/viewsets.py
+++ review/viewsets.py
@@ -1,4 +1,6 @@
-from rest_framework import viewsets
+from django.http import JsonResponse
+from django.template.defaulttags import comment
+from rest_framework import viewsets, request
 from django_filters import rest_framework as filters
 from django_filters import FilterSet
 from .models import Review
@@ -19,11 +21,11 @@ from rest_framework.generics import (
 
 
 class ReviewListViewSet(viewsets.ModelViewSet):
-    queryset = Review.objects.all().order_by('created_at')
+    queryset = Review.objects.all().order_by('-created_at')
     serializer_class = ReviewListSerializer
     # filterset_class = ReviewFilter
     filter_backends = (filters.DjangoFilterBackend,)
-    filter_fields = ('user','place')
+    filter_fields = ('user', 'place')
     http_method_names = ['get', 'post']
 
 
@@ -31,6 +33,14 @@ class ReviewUpdateViewSet(UpdateAPIView):
     queryset = Review.objects.all()
     serializer_class = ReviewUpdateSerializer
     lookup_field = 'id'
+
+    def patch(self, request, *args, **kwargs):
+        kwargs['partial'] = True
+        return self.partial_update(request, *args, **kwargs)
+
+    def partial_update(self, request, *args, **kwargs):
+        kwargs['partial'] = True
+        return self.update(request, *args, **kwargs)
 
 
 class ReviewDeleteViewSet(DestroyAPIView):
