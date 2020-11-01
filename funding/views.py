b97e299a43109dd9ca420d5171773b2d5820b4d6
yywon <57125912+hyywon@users.noreply.github.com>
2020-11-01 23:22:46 +0900

    기능::좋아요 API 수정(예원)

--- funding/views.py
+++ funding/views.py
@@ -1,6 +1,7 @@
 from requests import Response
 from django.http import HttpResponse
 from .models import Funding
+from accounts.models import User
 from rest_framework.generics import (
     ListCreateAPIView,
     UpdateAPIView,
@@ -65,10 +66,12 @@ def FundingLike(request, user, id):
         print(user, id)
         place = Funding.objects.get(id=id)
         print(place.user_likes.all().values('nickname'))
+        user_id = User.objects.get(nickname=user)
+        print(user_id)
         if place.user_likes.filter(nickname=user).exists():
-            place.user_likes.remove(user)
+            place.user_likes.remove(user_id)
             print("좋아요 취소")
         else:
-            place.user_likes.add(user)
+            place.user_likes.add(user_id)
             print("좋아요")
     return HttpResponse(status=200)
--- place/views.py
+++ place/views.py
@@ -1,17 +1,22 @@
 from .models import Place
+from accounts.models import User
 from django.http import HttpResponse
 
 
 def PlaceLike(request, user, q):
-
     if request.method == 'GET':
         print(user, q)
+
         place = Place.objects.get(place_id=q)
+        print(place.user_likes.all())
         print(place.user_likes.all().values('nickname'))
-        if place.user_likes.filter(nickname=user).exists():
-            place.user_likes.remove(user)
+        user_id = User.objects.get(nickname=user)
+        print(user_id)
+        if user_id in place.user_likes.all():
+            # place.user_likes.filter(nickname=user).exists():
+            place.user_likes.remove(user_id)
             print("좋아요 취소")
         else:
-            place.user_likes.add(user)
+            place.user_likes.add(user_id)
             print("좋아요")
     return HttpResponse(status=200)
