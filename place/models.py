df5060b01cb10ad0496b480bb28069b0c76b0048
yywon <57125912+hyywon@users.noreply.github.com>
2020-11-02 15:55:14 +0900

    기능:: backed_list 조회 User nickname 으로 수정(예원)

--- place/models.py
+++ place/models.py
@@ -27,16 +27,16 @@ BODY_TEMPERATURE_CHECK_CHOICES = (
 class Place(models.Model):
     # Place model 
     place_id = models.CharField(primary_key=True, max_length=50)
-    title = models.CharField(max_length=30,null=True,)
-    place_image = models.ImageField(upload_to="place/img/", blank=True, null=True,)
-    description = models.CharField(max_length=100, blank=True, null=True,)
+    title = models.CharField(max_length=50,null=True,)
+    place_image = models.ImageField(upload_to="place/img/", blank=False, null=True,)
+    description = models.CharField(max_length=1000, blank=True, null=True,)
     address = models.CharField(max_length=10000,null=True,)
     lng = models.CharField(max_length=50, null=True)
     lat = models.CharField(max_length=50, null=True)
     hand_sanitizer = models.PositiveIntegerField(choices=SANITIZER_CHOICES,default=2)
     person_hygiene = models.PositiveIntegerField(choices=PERSON_HYGIENE_CHOICES,default=2)
-    body_temperature_check = models.PositiveIntegerField( choices=BODY_TEMPERATURE_CHECK_CHOICES ,default=2)
-    counts = models.PositiveIntegerField(default=0,null=True,)
+    body_temperature_check = models.PositiveIntegerField( choices=BODY_TEMPERATURE_CHECK_CHOICES, default=2)
+    counts = models.PositiveIntegerField(default=0, null=True,)
     #  likes = models.ManyToManyField(User, related_name='place_likes', default=None, blank=True)
     # review_avg = models.FloatField(default=0, blank=True)
 
