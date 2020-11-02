d2eeca1588b18fa124ec5d230465c8377fa2e2ff
김율희 <dbfgml741@likelion.org>
2020-11-02 17:43:13 +0900

    (기능)::주요 펀딩 클래스 생성(율희)

--- funding/admin.py
+++ funding/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
-from .models import Funding
+from .models import Funding, MainFunding
 
 class FundingAdmin(admin.ModelAdmin):
     model = Funding
     filter_horizontal = ("backed_list",)
 
 admin.site.register(Funding, FundingAdmin)
+
+
+class MainFundingAdmin(admin.ModelAdmin):
+    model = MainFunding
+    filter_horizontal = ("main_funding",)
+
+admin.site.register(Funding, FundingAdmin)
+admin.site.register(MainFunding, MainFundingAdmin)
