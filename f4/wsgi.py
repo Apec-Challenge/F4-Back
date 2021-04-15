cc66873b9e2f6a3c1647c12b8e7e8be1b8cfd4a4
Chanjongp <pcj0198@daum.net>
2021-04-16 00:53:00 +0900

    wsgi file setting

--- f4/wsgi.py
+++ f4/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Project_F4.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'f4.settings')
 
 application = get_wsgi_application()
